1ad5b3c55bc3173d4132665fd121e53b6f268cfe
Alexander Van Damme <alexanderdebest@live.be>
2016-12-16 20:19:31 +0100

    fixed heigth

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -19,7 +19,7 @@
     return {
       restrict: 'AE',
       replace: 'true',
-      template: '<div class="flex-video widescreen"><iframe width=100% height=100% ng-src="{{post.link}}" frameborder="0" allowfullscreen></iframe></div>'
+      template: '<div class="flex-video widescreen"><iframe width=100% height="315" ng-src="{{post.link}}" frameborder="0" allowfullscreen></iframe></div>'
     };
   });
 
